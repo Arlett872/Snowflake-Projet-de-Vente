7adaf465109ea5f74c3dd2350944e25cb309c617
Arlett872 <dany.djiala@outlook.com>
2026-01-02 12:55:35 +0100

    Create 01_create_table.sql

---
+++ 01_create_table.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE TABLE sales (
+    sale_id INTEGER,
+    sale_date DATE,
+    customer_id INTEGER,
+    product STRING,
+    category STRING,
+    quantity INTEGER,
+    unit_price INTEGER,
+    region STRING
+);
